969139de716806a1f56e3b813912ae7585dc6619
Tom <thomas.jaeger987@outlook.com>
2026-02-20 11:50:11 +1030

    Added complete part 3

---
+++ Minimum Element/Minimum Element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int FindMinimum(int arr[], const int size);
+
+int main() {
+	const int arrSize = 20;
+	int array[arrSize] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
+
+	assert(FindMinimum(array, arrSize) == 2);
+
+	cout << "\n\n======END======\n";
+}
+
+int FindMinimum(int arr[], const int size) {
+	int min = arr[0];
+	for (int i = 1; i < size; i++)
+		if (arr[i] < min)
+			min = arr[i];
+	return min;
+}
